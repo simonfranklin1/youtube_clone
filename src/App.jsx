9bc4cb1b8c271e3172cfbbe41bafb911807f37a2
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2023-12-10 21:44:10 -0300

    Alterando configurações do react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
-import { Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { Box } from '@mui/material'
+import { ChannelDetail, Feed, SearchFeed, VideoDetail } from './routes'
 
 function App() {
 
   return (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
-      <Outlet />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
     </Box>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,39 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { ChannelDetail, Feed, SearchFeed, VideoDetail } from './routes'
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Feed />
-      },
-      {
-        path: "/video/:id",
-        element: <VideoDetail />
-      },
-      {
-        path: "/channel/:id",
-        element: <ChannelDetail />
-      },
-      {
-        path: "/search/:searchTerm",
-        element: <SearchFeed />
-      }
-    ]
-  }
-])
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
   </React.StrictMode>,
 )
