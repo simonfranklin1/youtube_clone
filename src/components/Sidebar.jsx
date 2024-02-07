643cd014a8901b86e707c4640953ff282152b30e
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-02-07 14:00:32 -0300

    Corrigindo estilo da imagem dos cards

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const Sidebar = ({selectedCategory, setSelectedCategory}) => {
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
   return (
     <Stack 
         sx={{ 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from '../utils/constants'
+import { demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from '../utils/constants'
 
 const VideoCard = ({ video }) => {
   const { id, snippet } = video
 
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%', sm: '100%' },
-      boxShadow: 'none', borderRadius: 0, display: { sm: 'flex', md: 'block', sm: 'flex'}, background: "#000"}}>
+    <Card sx={{
+      width: { md: '320px', xs: '100%', sm: '100%' },
+      boxShadow: 'none', borderRadius: 0, display: { sm: 'flex', md: 'block', sm: 'flex' }, background: "#000"
+    }}>
+      <Link to={id.videoId ? `/video/${id.videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          title={snippet?.title}
+          sx={{ width: {xs: '100%', sm: '370px', md: '100%'}, height: { xs: 180, sm: '200px', md: 180 }, borderRadius: '12px'}}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#000", height: '100px', flex: '1' }}>
         <Link to={id.videoId ? `/video/${id.videoId}` : demoVideoUrl}>
-            <CardMedia 
-                image={snippet?.thumbnails?.high?.url}
-                title={snippet?.title} 
-                sx={{ width: { xs: '100%', sm: '370px'}, height: { xs: 180, sm: '200px', md: 180}, borderRadius: "15px", objectFit: "contain"}}/>
+          <Typography variant='subtitle1' fontWeight={'bold'} color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
         </Link>
-        <CardContent sx={{ backgroundColor: "#000", height: '100px', flex: '1'}}>
-          <Link to={id.videoId ? `/video/${id.videoId}` : demoVideoUrl}>
-            <Typography variant='subtitle1' fontWeight={'bold'} color="#fff">
-              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-            </Typography>
-          </Link>
-          <Link to={snippet?.channelId ? `channel/${snippet?.channelId}` : demoChannelUrl}>
-            <Typography variant='subtitle2' fontWeight={'bold'} color="gray">
-              {snippet?.channelTitle.slice(0, 60) || demoChannelTitle}
-              <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px'}} />
-            </Typography>
-          </Link>
-        </CardContent>
+        <Link to={snippet?.channelId ? `channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' fontWeight={'bold'} color="gray">
+            {snippet?.channelTitle.slice(0, 60) || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
